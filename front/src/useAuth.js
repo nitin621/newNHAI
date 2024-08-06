4bc7d55b3f689fdc4df3f1d8156f22affcf9ac2d
nitin621 <70137363+nitin621@users.noreply.github.com>
2024-08-06 12:09:18 +0530

    Update useAuth.js

--- front/src/useAuth.js
+++ front/src/useAuth.js
@@ -3,8 +3,8 @@
 import { useState, useEffect, useRef, useCallback } from "react";
 import Keycloak from "keycloak-js";
 
-var KEYCLOAK_URL = "http://10.3.0.12";
-var REACT_URL = "http://10.3.0.12/react";
+var KEYCLOAK_URL = "http://10.3.0.12:8080";
+var REACT_URL = "http://10.3.0.12:3000";
 
 // var KEYCLOAK_URL = "";
 // var REACT_URL = "http://10.3.0.12";
